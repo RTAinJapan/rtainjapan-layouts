d53de7b32162fae6a60a9cfdb9b8131d9fb393ab
Hoishin <hoishinxii@gmail.com>
2017-10-27 23:04:04 +0900

    Extension: retrieving schedule from horaro

--- extension/index.js
+++ extension/index.js
@@ -1,5 +1,13 @@
-'use strict';
+const request = require('superagent');
 
 module.exports = function (nodecg) {
-
+	request
+		.get(`https://horaro.org/-/api/v1/schedules/${nodecg.bundleConfig.horaro.scheduleId}`)
+		.end((err, res) => {
+			if (err) {
+				nodecg.log.error(err);
+			} else {
+				console.log(res.body.data.items[0]);
+			}
+		});
 };
