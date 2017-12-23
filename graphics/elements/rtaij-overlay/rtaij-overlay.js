bec4cb16919d374662af644ddfef32981a1b2dde
Hoishin <hoishinxii@gmail.com>
2017-12-23 21:02:13 +0900

    Add layout prototypes

---
+++ graphics/elements/rtaij-overlay/rtaij-overlay.js
@@ -0,0 +1,9 @@
+(function () {
+	class RtaijOverlay extends Polymer.Element {
+		static get is() {
+			return 'rtaij-overlay';
+		}
+	}
+
+	customElements.define(RtaijOverlay.is, RtaijOverlay);
+})();
