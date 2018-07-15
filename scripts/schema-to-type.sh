3da89cd2852eb0fe789b5f8f02b8317cff02f8b9
hoishin <hoishinxii@gmail.com>
2018-07-15 17:22:34 +0900

    dont output type definition

--- scripts/schema-to-type.sh
+++ scripts/schema-to-type.sh
@@ -1,6 +1,6 @@
-json2ts configschema.json > src/types/configschema.ts
+json2ts configschema.json > src/types/configschema.d.ts
 
 for f in $(find schemas -name "*.json" | sed -r 's/^(.*)\.json$/\1/')
 do
-	json2ts $f.json > src/types/$f.ts
+	json2ts $f.json > src/types/$f.d.ts
 done
