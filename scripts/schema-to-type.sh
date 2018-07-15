9b42913ecc71fe3e440ee021792c2c01d6d6f52b
hoishin <hoishinxii@gmail.com>
2018-07-15 20:26:10 +0900

    fix schema to TS build script

--- scripts/schema-to-type.sh
+++ scripts/schema-to-type.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-json2ts configschema.json > src/types/configschema.d.ts
+npx json2ts configschema.json > src/types/configschema.d.ts
 
 for f in $(find schemas -name "*.json" | sed -r 's/^(.*)\.json$/\1/')
 do
-	json2ts $f.json > src/types/$f.d.ts
+	npx json2ts $f.json > src/types/$f.d.ts
 done
