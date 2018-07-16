78e493f828a849fc1fdfc907fb478f7d75526370
hoishin <hoishinxii@gmail.com>
2018-07-17 03:11:02 +0900

    move time-object out of extension directory

--- src/extension/timekeeping.ts
+++ src/extension/timekeeping.ts
@@ -1,7 +1,7 @@
-import {TimeObject, TimerState} from './classes/time-object';
 import {NodeCG} from '../types/nodecg';
 import {CurrentRun} from '../types/schemas/currentRun';
 import {ChecklistCompleted} from '../types/schemas/checklistCompleted';
+import { TimeObject, TimerState } from '../lib/time-object';
 
 const TRY_TICK_INTERVAL = 10;
 
