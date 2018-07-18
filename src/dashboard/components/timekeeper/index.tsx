d719606e7e1192270f2d12bc5c168c3b0ac87ba3
hoishin <hoishinxii@gmail.com>
2018-07-19 00:25:14 +0900

    add timer's control buttons

--- src/dashboard/components/timekeeper/index.tsx
+++ src/dashboard/components/timekeeper/index.tsx
@@ -1,27 +1,69 @@
 import React from 'react';
 import styled from 'styled-components';
 import cloneDeep from 'lodash/cloneDeep';
+
+// MUI Core
+import {Color} from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import green from '@material-ui/core/colors/green';
+import orange from '@material-ui/core/colors/orange';
+import purple from '@material-ui/core/colors/purple';
+import grey from '@material-ui/core/colors/grey';
+import PlayArrow from '@material-ui/icons/PlayArrow';
+import Pause from '@material-ui/icons/Pause';
+import Refresh from '@material-ui/icons/Refresh';
+import ModeEdit from '@material-ui/icons/ModeEdit';
+
+import {TimeObject, TimerState} from '../../../lib/time-object';
 import {
 	stopwatchRep,
 	currentRunRep,
 	checklistCompleteRep,
 } from '../../replicants';
-import {TimeObject, TimerState} from '../../../lib/time-object';
 
 const Container = styled.div`
-	margin: 16px;
+	margin: 0 16px;
 	font-weight: 700;
 	display: grid;
-	grid-template-columns: auto 1fr;
+	grid-template-columns: 1fr auto;
 	grid-template-rows: 105px 1fr;
-	grid-area: 'timer ctrls' 'runners';
+	grid-template-areas: 'timer ctrls' 'runners runners';
+	justify-items: center;
+	align-items: center;
 `;
 
 const Timer = styled.div`
-	grid-area: 'timer';
+	grid-area: timer;
+	padding: 0 16px;
 	font-size: 55px;
-	text-align: center;
 `;
+
+const CtrlsContainer = styled.div`
+	grid-area: ctrls;
+	display: grid;
+	grid-template-columns: 1fr 1fr;
+	grid-template-rows: 1fr 1fr;
+	gap: 8px;
+	justify-items: center;
+	align-items: center;
+`;
+
+const customPrimaryColorTheme = (color: Color) =>
+	createMuiTheme({
+		props: {
+			MuiButton: {
+				size: 'large',
+				fullWidth: true,
+				variant: 'raised',
+				color: 'primary',
+			},
+		},
+		palette: {
+			primary: color,
+		},
+	});
 
 export class Timekeeper extends React.Component<
 	{},
@@ -69,7 +111,28 @@ export class Timekeeper extends React.Component<
 		return (
 			<Container>
 				<Timer>{this.state.timer.formatted}</Timer>
-				<div>hoge</div>
+				<CtrlsContainer>
+					<MuiThemeProvider theme={customPrimaryColorTheme(green)}>
+						<Button>
+							<PlayArrow />開始
+						</Button>
+					</MuiThemeProvider>
+					<MuiThemeProvider theme={customPrimaryColorTheme(orange)}>
+						<Button>
+							<Pause />停止
+						</Button>
+					</MuiThemeProvider>
+					<MuiThemeProvider theme={customPrimaryColorTheme(purple)}>
+						<Button>
+							<Refresh />リセット
+						</Button>
+					</MuiThemeProvider>
+					<MuiThemeProvider theme={customPrimaryColorTheme(grey)}>
+						<Button>
+							<ModeEdit />編集
+						</Button>
+					</MuiThemeProvider>
+				</CtrlsContainer>
 				<div>hoge</div>
 				<div>hoge</div>
 				<div>hoge</div>
