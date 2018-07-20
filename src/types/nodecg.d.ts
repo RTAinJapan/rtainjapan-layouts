440215abaaa0e78e873dfbcfcc96fc01da35cc53
hoishin <hoishinxii@gmail.com>
2018-07-21 01:32:43 +0900

    more nodecg typedef

--- src/types/nodecg.d.ts
+++ src/types/nodecg.d.ts
@@ -1,9 +1,15 @@
-import { Configschema } from "./configschema";
+import {Configschema} from './configschema';
 
 export interface NodeCG {
-	Replicant<T = never>(name: string, options?: {defaultValue: T}): Replicant<T>;
+	Replicant<T = never>(
+		name: string,
+		options?: {defaultValue: T}
+	): Replicant<T>;
 	sendMessage<T = never, U = never>(message: string, data: T): Promise<U>;
-	listenFor<T = never>(message: string, handler: (data: T, cb?: () => void) => void): void;
+	listenFor<T = never, U extends boolean = boolean>(
+		message: string,
+		handler: (data: T, cb: ListenForCbObj<U>) => void
+	): void;
 	log: Logger;
 	Logger: new (label: string) => Logger;
 	bundleName: string;
@@ -13,7 +19,7 @@ export interface NodeCG {
 
 export interface Replicant<T> {
 	value: T;
-	on(event: 'change', handler: (newVal: T, oldVal: T) => void): void
+	on(event: 'change', handler: (newVal: T, oldVal: T) => void): void;
 }
 
 export interface Logger {
@@ -30,5 +36,18 @@ export interface NodeCGConfig {
 			scope: string;
 			clientID: string;
 		};
-	}
+	};
+}
+
+export type ListenForCbObj<T extends boolean> = T extends true
+	? HandledListenForCb<T>
+	: UnhandledListenForCb<T>;
+
+interface HandledListenForCb<T extends boolean> {
+	handled: T
+}
+
+interface UnhandledListenForCb<T extends boolean> {
+	(...args: any[]): void
+	handled: T
 }
