2010e1f0f9395512bd9e5f45ef4cb38823f1db5e
hoishin <hoishinxii@gmail.com>
2018-07-21 07:37:48 +0900

    remove sass

--- src/dashboard/index.tsx
+++ src/dashboard/index.tsx
@@ -1,9 +1,12 @@
 import '../lib/react-devtools';
 import React from 'react';
 import ReactDom from 'react-dom';
+
 import {App} from './app';
 import {twitterCallback} from './lib/twitter-callback';
 
-twitterCallback();
+document.body.style.margin = '0'
+document.body.style.padding = '0'
 
+twitterCallback();
 ReactDom.render(<App />, document.getElementById('app'));
