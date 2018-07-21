28f3dab486b649685450d2b54fe52f4644265979
hoishin <hoishinxii@gmail.com>
2018-07-22 04:49:03 +0900

    schema output modify

---
+++ scripts/schema-types.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+const {compileFromFile} = require('json-schema-to-typescript');
+const appRoot = require('app-root-path');
+
+const writeFilePromise = util.promisify(fs.writeFile);
+
+const outDir = appRoot.resolve('types/schemas');
+const schemasDir = appRoot.resolve('schemas');
+const schemas = fs.readdirSync(schemasDir).filter(f => f.endsWith('.json'));
+
+const compile = (input, output) =>
+	compileFromFile(input)
+		.then(ts => writeFilePromise(output, ts))
+		.catch(err => {
+			console.error(err);
+		});
+
+compile(
+	appRoot.resolve('configschema.json'),
+	path.resolve(outDir, 'configschema.d.ts')
+);
+
+for (const schema of schemas) {
+	compile(
+		path.resolve(schemasDir, schema),
+		path.resolve(outDir, schema.replace(/\.json/i, '.d.ts'))
+	);
+}
