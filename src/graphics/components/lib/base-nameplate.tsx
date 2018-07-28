eaf9407d19eee6a1388df3772996b88ce267ecbb
hoishin <hoishinxii@gmail.com>
2018-07-29 04:17:36 +0900

    combine components

--- src/graphics/components/lib/base-nameplate.tsx
+++ src/graphics/components/lib/base-nameplate.tsx
@@ -1,7 +1,7 @@
 import React, {ReactNode} from 'react';
+import delay from 'delay';
 import {CurrentRun} from '../../../../types/schemas/currentRun';
 import {currentRunRep} from '../../../lib/replicants';
-import {SocialType, SocialContainer} from './social-container';
 import {
 	SubContainer,
 	LabelIcon,
@@ -9,7 +9,27 @@ import {
 	Name,
 	Ruler,
 	Container,
+	SocialInfo,
 } from './nameplate';
+import styled from '../../../../node_modules/styled-components';
+import twitchIcon from '../../images/icon/twitch.png';
+import nicoIcon from '../../images/icon/nico.png';
+import twitterIcon from '../../images/icon/twitter.png';
+
+const enum SocialType {
+	Twitch,
+	Nico,
+	Twitter,
+}
+
+interface Props {
+	index?: number;
+}
+
+interface State {
+	runners: CurrentRun['runners'];
+	socialType: SocialType;
+}
 
 const StyledContainer = Container.extend`
 	position: absolute;
@@ -22,13 +42,16 @@ const StyledContainer = Container.extend`
 	z-index: 1;
 `;
 
-interface Props {
-	index?: number;
-}
-
-interface State {
-	runners: CurrentRun['runners'];
-}
+const SocialContainer = styled.div`
+	height: 100%;
+	position: absolute;
+	display: flex;
+	flex-flow: row nowrap;
+	justify-content: flex-start;
+	align-items: flex-end;
+	opacity: 1;
+	transition: opacity 0.33s linear;
+`;
 
 export abstract class BaseNameplate extends React.Component<Props, State> {
 	protected abstract readonly currentRunChangeHandler: (
@@ -38,14 +61,18 @@ export abstract class BaseNameplate extends React.Component<Props, State> {
 	protected abstract readonly rootId: string;
 	protected abstract readonly label: string;
 
-	state: State = {runners: undefined};
+	state: State = {runners: [], socialType: SocialType.Twitch};
+
+	private readonly _currentRunChanged = (newVal: CurrentRun) => {
+		this.currentRunChangeHandler(newVal);
+	};
 
 	componentDidMount() {
-		currentRunRep.on('change', this.currentRunChangeHandler);
+		currentRunRep.on('change', this._currentRunChanged);
 	}
 
 	componentWillUnmount() {
-		currentRunRep.removeListener('change', this.currentRunChangeHandler);
+		currentRunRep.removeListener('change', this._currentRunChanged);
 	}
 
 	protected readonly Container = (props: {children?: ReactNode}) => (
@@ -54,12 +81,26 @@ export abstract class BaseNameplate extends React.Component<Props, State> {
 				<LabelIcon src={this.iconPath} />
 				<Label>{this.label}</Label>
 				<Name>{this.name}</Name>
-				<SocialContainer socialInfo={this.socialInfo} />
+				<SocialContainer>
+					<img src={this.iconSrc} />
+					<SocialInfo>{this.targetRunner.name}</SocialInfo>
+				</SocialContainer>
 			</SubContainer>
 			{props.children}
 			<Ruler />
 		</StyledContainer>
 	);
+
+	private get iconSrc() {
+		switch (this.state.socialType) {
+			case SocialType.Nico:
+				return nicoIcon;
+			case SocialType.Twitch:
+				return twitchIcon;
+			case SocialType.Twitter:
+				return twitterIcon;
+		}
+	}
 
 	protected get socialInfo() {
 		const runner = this.targetRunner;
