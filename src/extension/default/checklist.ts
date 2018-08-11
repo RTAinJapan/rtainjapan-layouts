6068af47fb2a0dda86516cb81c5f74408a9ae3aa
hoishin <hoishinxii@gmail.com>
2018-08-11 15:20:46 +0900

    update checklist

--- src/extension/default/checklist.ts
+++ src/extension/default/checklist.ts
@@ -1,13 +1,9 @@
 export default [
 	{name: '走者の情報を確認', complete: false},
 	{name: 'ゲームの情報を確認', complete: false},
-	{name: '技術メモ確認', complete: false},
-	{name: 'レイアウトがあっている', complete: false},
-	{name: 'カメラ確認', complete: false},
+	{name: '正しいレイアウト', complete: false},
 	{name: '走者とゲーム画面が一致', complete: false},
 	{name: 'ゲーム画面をクロップ', complete: false},
-	{name: 'アニメーション問題なし', complete: false},
 	{name: 'マイクとゲーム音の調整', complete: false},
-	{name: '録画再起動', complete: false},
 	{name: '走者にゴーサイン', complete: false},
 ];
