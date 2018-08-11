0e2774e3acf9bf743be8a252761f2c583cbc6ba9
hoishin <hoishinxii@gmail.com>
2018-08-11 15:49:02 +0900

    complete break scene

--- src/graphics/views/break.tsx
+++ src/graphics/views/break.tsx
@@ -1,34 +1,120 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import dot1 from '../images/break/dot1.png';
-import dot2 from '../images/break/dot2.png';
-import dot3 from '../images/break/dot3.png';
-import dot4 from '../images/break/dot4.png';
-import rtachan from '../images/break/rtachan.png';
+import styled from 'styled-components';
 
-const App = () => (
-	<div className="bg">
-		<img src={dot1} className="deep1" />
-		<img src={dot2} className="deep2" />
-		<img src={dot3} className="deep3" />
-		<img src={dot4} className="deep4" />
-		<img src={dot4} className="deep5" />
-		<img src={dot3} className="deep6" />
-		<img src={dot2} className="deep7" />
-		<img src={dot1} className="deep8" />
-		<img src={dot1} className="pale1" />
-		<img src={dot2} className="pale2" />
-		<img src={dot3} className="pale3" />
-		<img src={dot4} className="pale4" />
-		<img src={dot4} className="pale5" />
-		<img src={dot3} className="pale6" />
-		<img src={dot2} className="pale7" />
-		<img src={dot1} className="pale8" />
-		<img src={rtachan} className="rtachan" />
-	</div>
-);
+import {RtaijOverlay} from '../components/rtaij-overlay';
+import notification from '../images/break/notification.png';
+import {BreakBackground} from '../components/break-background';
+import {Schedule} from '../../../types/schemas/schedule';
+import {scheduleRep} from '../../lib/replicants';
+import {CurrentRun} from '../../../types/schemas/currentRun';
+import nodecg from '../../lib/nodecg';
+import {UpcomingRun} from '../components/upcoming-run';
 
-ReactDom.render(<App />, document.getElementById('break'), () => {
+const NotificationIcon = styled.img.attrs({src: notification})`
+	position: absolute;
+	left: 60px;
+	bottom: 15px;
+`;
+
+const NotificationText = styled.div`
+	position: absolute;
+	left: 225px;
+	bottom: 0px;
+	height: 150px;
+
+	color: #ffffff;
+	font-family: 'MigMix 2P';
+	font-weight: bold;
+	font-size: 45px;
+	line-height: 120%;
+
+	display: grid;
+	align-content: center;
+`;
+
+const UpcomingTitle = styled.div`
+	position: absolute;
+	top: 200px;
+	left: 60px;
+	width: 180px;
+
+	background-color: #4d7dff;
+	box-shadow: 4.5px 4.5px 6px -1.5px rgba(0, 0, 0, 1);
+
+	font-family: 'MigMix 2P';
+	font-weight: bold;
+	font-size: 30px;
+	line-height: 45px;
+	color: #ffffff;
+	text-align: center;
+`;
+
+const UpcomingContainer = styled.div`
+	position: absolute;
+	top: ${150 + 50 + 45 + 70}px;
+	left: 120px;
+
+	display: grid;
+	grid-auto-flow: row;
+	grid-gap: 70px;
+	justify-items: start;
+`;
+
+interface State {
+	upcomingRuns: CurrentRun[];
+}
+
+class Break extends React.Component<{}, State> {
+	public state: State = {upcomingRuns: []};
+
+	public render() {
+		return (
+			<div>
+				<BreakBackground />
+				<RtaijOverlay
+					bottomHeightPx={150}
+					isBreak
+					TweetProps={{rowDirection: true}}
+				/>
+				<NotificationIcon />
+				<NotificationText>
+					準備中です、しばらくお待ち下さい
+				</NotificationText>
+				<UpcomingTitle>今後の予定</UpcomingTitle>
+				<UpcomingContainer>
+					{this.state.upcomingRuns.map(run => (
+						<UpcomingRun key={run.pk} {...run} />
+					))}
+				</UpcomingContainer>
+			</div>
+		);
+	}
+
+	public componentDidMount() {
+		scheduleRep.on('change', this.updateUpcoming);
+	}
+
+	public componentWillUnmount() {
+		scheduleRep.removeListener('change', this.updateUpcoming);
+	}
+
+	private readonly updateUpcoming = () => {
+		nodecg.readReplicant<CurrentRun>('currentRun', currentRun => {
+			const currentRunIndex = currentRun.index || 0;
+			nodecg.readReplicant<Schedule>('schedule', schedule => {
+				this.setState({
+					upcomingRuns: schedule.slice(
+						currentRunIndex,
+						currentRunIndex + 3
+					),
+				});
+			});
+		});
+	};
+}
+
+ReactDom.render(<Break />, document.getElementById('break'), () => {
 	setTimeout(() => {
 		document.body.style.opacity = '1';
 	}, 1000);
