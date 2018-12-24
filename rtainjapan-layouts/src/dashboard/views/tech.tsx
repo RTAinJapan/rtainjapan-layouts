c6430c4c03007e90b0a7c34cb968002b913896ed
Hoishin <hoishinxii@gmail.com>
2018-12-25 06:16:55 +0900

    temporary hide components

--- rtainjapan-layouts/src/dashboard/views/tech.tsx
+++ rtainjapan-layouts/src/dashboard/views/tech.tsx
@@ -6,10 +6,10 @@ import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import React from 'react';
 import ReactDom from 'react-dom';
 import styled from 'styled-components';
-import {Checklist} from '../components/checklist';
-import {Schedule} from '../components/schedule';
+// import {Checklist} from '../components/checklist';
+// import {Schedule} from '../components/schedule';
 import {Timekeeper} from '../components/timekeeper';
-import {Twitter} from '../components/twitter';
+// import {Twitter} from '../components/twitter';
 import {twitterCallback} from '../lib/twitter-callback';
 
 const Container = styled.div`
@@ -50,14 +50,10 @@ export const App = () => (
 		<Container>
 			<LeftColumn>
 				<Timekeeper />
-				<Checklist />
+				{/* <Checklist /> */}
 			</LeftColumn>
-			<Column>
-				<Schedule />
-			</Column>
-			<Column>
-				<Twitter />
-			</Column>
+			<Column>{/* <Schedule /> */}</Column>
+			<Column>{/* <Twitter /> */}</Column>
 		</Container>
 	</MuiThemeProvider>
 );
