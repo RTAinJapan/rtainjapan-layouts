4d94d8d73af917343e8904cb9776bb15554a445b
Keiichiro Amemiya <hoishinxii@gmail.com>
2019-12-27 15:52:24 +0900

    update checklist

--- src/extension/default/checklist.ts
+++ src/extension/default/checklist.ts
@@ -1,13 +1,10 @@
 let i = 1;
 export default [
-	{name: `${i++}. 音楽⬆ ゲームDiscord音⬇`, complete: false},
-	{name: `${i++}. ゲーム画面を非表示にする`, complete: false},
-	{name: `${i++}. OBSで次のレイアウトを選ぶ`, complete: false},
-	{name: `${i++}. Mixerチャンネルを選択する`, complete: false},
+	{name: `${i++}. 走者とゲームの情報を確認`, complete: false},
+	{name: `${i++}. 正しいレイアウトを選択`, complete: false},
+	{name: `${i++}. 走者とゲーム画面が一致`, complete: false},
+	{name: `${i++}. クロップをリセット`, complete: false},
 	{name: `${i++}. ゲーム画面をクロップ`, complete: false},
-	{name: `${i++}. 「対話」からゲーム音を出す`, complete: false},
-	{name: `${i++}. Discordとゲーム音の調整`, complete: false},
-	{name: `${i++}. ゲームDiscord音⬆ 音楽⬇`, complete: false},
-	{name: `${i++}. トランジションする`, complete: false},
-	{name: `${i++}. 走者にゴーサイン`, complete: false},
+	{name: `${i++}. Steam通知ポップアップOFF`, complete: false},
+	{name: `${i++}. 音声の準備ができた`, complete: false},
 ];
