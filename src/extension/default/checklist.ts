945b3c1156c54166226d5f05a4b37017321e5af5
Keiichiro Amemiya <hoishinxii@gmail.com>
2020-12-26 21:53:03 +0900

    fix: update checklist

--- src/extension/default/checklist.ts
+++ src/extension/default/checklist.ts
@@ -2,9 +2,10 @@ let i = 1;
 export default [
 	{name: `${i++}. 音楽↑ ゲームDiscord↓`, complete: false},
 	{name: `${i++}. OBSで次のレイアウト`, complete: false},
-	{name: `${i++}. RTMPの配信キー入力`, complete: false},
-	{name: `${i++}. クロップをリセット`, complete: false},
+	{name: `${i++}. NodeCGで配信ID入力`, complete: false},
+	{name: `${i++}. カメラの有無を入力`, complete: false},
 	{name: `${i++}. ゲーム画面をクロップ`, complete: false},
+	{name: `${i++}. (あったら)カメラをクロップ`, complete: false},
 	{name: `${i++}. Discordとゲーム音調整`, complete: false},
 	{name: `${i++}. ゲームDiscord↑ 音楽↓`, complete: false},
 	{name: `${i++}. トランジション`, complete: false},
