71dfa815a219a765fc6ca48019110dc122f08502
Keiichiro Amemiya <hoishinxii@gmail.com>
2020-12-29 05:01:23 +0900

    chore: print out twitter stream error

--- src/extension/twitter.ts
+++ src/extension/twitter.ts
@@ -82,6 +82,7 @@ export const twitter = async (nodecg: NodeCG) => {
 				streamLogger.warn('connecting');
 			});
 			stream.on('reconnect', (_req, _res, connectInterval: number) => {
+				console.log(_res);
 				// Twitter is having problems or we get rate limited. Reconnetion scheduled.
 				streamLogger.warn(`Reconnecting in ${connectInterval}ms`);
 			});
