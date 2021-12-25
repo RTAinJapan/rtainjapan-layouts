bc41224d86465d1e54a198b84fa54202bec388cf
Keiichiro Amemiya <hoishinxii@gmail.com>
2021-12-26 07:12:15 +0900

    feat: setup for better omnibar

--- src/browser/graphics/views/omnibar.tsx
+++ src/browser/graphics/views/omnibar.tsx
@@ -6,7 +6,16 @@ import {BoldText, ThinText, TimerText} from "../components/lib/text";
 import arrowImage from "../images/footer_arrow.svg";
 import lineImage from "../images/footer_line.svg";
 import {useReplicant} from "../../use-replicant";
-import {CSSProperties, useEffect, useRef, useState} from "react";
+import {
+	CSSProperties,
+	forwardRef,
+	ReactElement,
+	RefObject,
+	useCallback,
+	useEffect,
+	useRef,
+	useState,
+} from "react";
 import {text} from "../styles/colors";
 
 const donationTextContainerStyle: CSSProperties = {
@@ -30,39 +39,13 @@ const bounceEase = (progress: number) => {
 	return (-1 / 160) * x ** 2 + (3 / 8) * x - 23 / 5;
 };
 
-const Omnibar = () => {
-	const sponsorAssets = useReplicant("assets:charity-logo");
-	const textRef = useRef<HTMLDivElement>(null);
-	const [mainText, setMainText] = useState("");
-
-	useEffect(() => {
-		const textLoop = [
-			"国境なき医師団への寄付は donate.rtain.jp から",
-			"イベント中のTwitchの収益も国境なき医師団に寄付されます",
-		];
-		const tl = gsap.timeline({repeat: -1});
-		for (const text of textLoop) {
-			tl.call(() => {
-				setMainText(text);
-			});
-			tl.fromTo(textRef.current, {opacity: 0}, {opacity: 1, duration: 0.5});
-			tl.fromTo(
-				textRef.current,
-				{opacity: 1},
-				{opacity: 0, duration: 0.5},
-				"+=60",
-			);
-		}
-		return () => {
-			tl.kill();
-		};
-	}, []);
-
+const DonationTotal = () => {
 	const [donationTotal, setDonationTotal] = useState(0);
 	const [donationAmount, setDonationAmount] = useState(0);
 	const amountTextRef = useRef<HTMLDivElement>(null);
 	const donationTotalRef = useRef<HTMLDivElement>(null);
 
+	// donation total animation
 	useEffect(() => {
 		const tl = gsap.timeline();
 		nodecg.readReplicant("donation-total", (total) => {
@@ -80,26 +63,124 @@ const Omnibar = () => {
 			tl.call(() => {
 				setDonationAmount(amount);
 			});
-			tl.to(totalRef, {transform: "translateY(-150%)", duration: 14 / 60});
-			tl.to(
-				amountRef,
-				{
-					transform: "translateY(0%)",
-					duration: 32 / 60,
-					ease: bounceEase,
-				},
-				"<",
-			);
-			tl.set(totalRef, {transform: "translateY(150%)"});
+			tl.to(totalRef, {yPercent: -150, duration: 14 / 60});
+			tl.to(amountRef, {yPercent: 0, duration: 32 / 60, ease: bounceEase}, "<");
+			tl.set(totalRef, {yPercent: 150});
 			tl.call(() => {
 				setDonationTotal(total);
 			});
 
-			tl.to(totalRef, {transform: "translateY(0%)", duration: 0.5}, ">+3");
-			tl.to(amountRef, {transform: "translateY(-150%)", duration: 0.5}, "<");
-			tl.set(amountRef, {transform: "translateY(150%)"});
+			tl.to(totalRef, {yPercent: 0, duration: 0.5}, ">+3");
+			tl.to(amountRef, {yPercent: -150, duration: 0.5}, "<");
+			tl.set(amountRef, {yPercent: 150});
 		});
 	}, []);
+
+	return (
+		<div
+			style={{
+				gridColumn: "4 / 5",
+				gridRow: "1 / 2",
+				display: "grid",
+				gridAutoFlow: "column",
+				placeContent: "center",
+				alignItems: "center",
+				justifyItems: "end",
+			}}
+		>
+			<div ref={donationTotalRef} style={{...donationTextContainerStyle}}>
+				<BoldText style={{fontSize: "28px"}}>¥</BoldText>
+				<TimerText style={{fontSize: "36px"}}>
+					{donationTotal?.toLocaleString()}
+				</TimerText>
+			</div>
+			<div
+				ref={amountTextRef}
+				style={{
+					...donationTextContainerStyle,
+					transform: "translateY(150%)",
+				}}
+			>
+				<BoldText style={{fontSize: "34px"}}>+</BoldText>
+				<TimerText style={{fontSize: "36px"}}>
+					{donationAmount.toLocaleString()}
+				</TimerText>
+			</div>
+		</div>
+	);
+};
+
+const move = 50;
+const duration = 0.5;
+const hold = 60;
+
+const Row = forwardRef<
+	HTMLDivElement,
+	{header: string; children?: ReactElement}
+>((props, ref) => {
+	return (
+		<div
+			ref={ref}
+			style={{
+				gridColumn: "2 / 3",
+				gridRow: "1 / 2",
+				alignItems: "center",
+				display: "grid",
+				gridTemplateColumns: "auto auto 1fr",
+			}}
+		>
+			<ThinText style={{fontSize: "24px"}}>{props.header}</ThinText>
+			<img src={arrowImage}></img>
+			{props.children}
+		</div>
+	);
+});
+
+const Omnibar = () => {
+	const sponsorAssets = useReplicant("assets:charity-logo");
+
+	const [tl, setTl] = useState<gsap.core.Timeline>();
+	useEffect(() => {
+		const tl = gsap.timeline({repeat: -1});
+		setTl(tl);
+		return () => {
+			tl.kill();
+		};
+	}, []);
+
+	const oshiraseRef = useRef<HTMLDivElement>(null);
+	const bidwarRef = useRef<HTMLDivElement>(null);
+	const donationCommentRef = useRef<HTMLDivElement>(null);
+
+	const donateLinkTextRef = useRef<HTMLDivElement>(null);
+	const twitchRevenueTextRef = useRef<HTMLDivElement>(null);
+
+	const swapContent = useCallback(
+		(
+			previous: RefObject<HTMLElement> | undefined,
+			next: RefObject<HTMLElement>,
+		) => {
+			const tl = gsap.timeline();
+			tl.fromTo(next.current, {y: move}, {y: 0, duration});
+			if (previous) {
+				tl.fromTo(previous.current, {y: 0}, {y: -move, duration}, "<");
+			}
+			return tl;
+		},
+		[],
+	);
+
+	useEffect(() => {
+		gsap.set([bidwarRef.current, donationCommentRef.current], {y: move});
+	}, []);
+
+	useEffect(() => {
+		if (!tl) {
+			return;
+		}
+		tl.add(swapContent(donateLinkTextRef, twitchRevenueTextRef), `+=${hold}`);
+		tl.add(swapContent(twitchRevenueTextRef, donateLinkTextRef), `+=${hold}`);
+	}, [tl, swapContent]);
 
 	return (
 		<div
@@ -111,49 +192,44 @@ const Omnibar = () => {
 				top: 0,
 				left: 0,
 				display: "grid",
-				gridTemplateColumns: "15px auto auto 1fr auto auto auto",
+				gridTemplateColumns: "15px 1fr auto auto auto",
+				gridTemplateRows: "50px",
 				placeContent: "stretch",
 				alignItems: "center",
 				color: text.omnibar,
 				backgroundColor: "rgba(255,255,255,0.8)",
 			}}
 		>
-			<div>{/* empty */}</div>
-			<ThinText style={{fontSize: "24px"}}>RTA in Japanからのお知らせ</ThinText>
-			<img src={arrowImage}></img>
-			<ThinText ref={textRef} style={{fontSize: "24px"}}>
-				{mainText}
-			</ThinText>
-			<img src={lineImage}></img>
-			<div
-				style={{
-					display: "grid",
-					gridAutoFlow: "column",
-					placeContent: "center",
-					alignItems: "center",
-					justifyItems: "end",
-				}}
-			>
-				<div ref={donationTotalRef} style={{...donationTextContainerStyle}}>
-					<BoldText style={{fontSize: "28px"}}>¥</BoldText>
-					<TimerText style={{fontSize: "36px"}}>
-						{donationTotal?.toLocaleString()}
-					</TimerText>
+			<Row header='RTA in Japanからのお知らせ' ref={oshiraseRef}>
+				<div style={{display: "grid"}}>
+					<ThinText
+						style={{fontSize: "24px", gridColumn: "1 / 2", gridRow: "1 / 2"}}
+						ref={donateLinkTextRef}
+					>
+						国境なき医師団への寄付は donate.rtain.jp から
+					</ThinText>
+					<ThinText
+						style={{fontSize: "24px", gridColumn: "1 / 2", gridRow: "1 / 2"}}
+						ref={twitchRevenueTextRef}
+					>
+						イベント中のTwitchの収益も国境なき医師団に寄付されます
+					</ThinText>
 				</div>
-				<div
-					ref={amountTextRef}
-					style={{
-						...donationTextContainerStyle,
-						transform: "translateY(150%)",
-					}}
-				>
-					<BoldText style={{fontSize: "34px"}}>+</BoldText>
-					<TimerText style={{fontSize: "36px"}}>
-						{donationAmount.toLocaleString()}
-					</TimerText>
-				</div>
-			</div>
-			<img src={sponsorAssets?.[0]?.url}></img>
+			</Row>
+			<Row header='寄付額投票' ref={bidwarRef}></Row>
+			<Row header='寄付コメント' ref={donationCommentRef}></Row>
+
+			<img
+				src={lineImage}
+				style={{gridColumn: "3 / 4", gridRow: "1 / 2"}}
+			></img>
+
+			<DonationTotal></DonationTotal>
+
+			<img
+				src={sponsorAssets?.[0]?.url}
+				style={{gridColumn: "5 / 6", gridRow: "1 / 2"}}
+			></img>
 		</div>
 	);
 };
