476d6595c745581b4a106d2d38fe3d49e53172f0
yagamuu <ygmpeer@gmail.com>
2023-04-28 00:14:16 +0900

    ダッシュボードのカメラ情報入力フォームを改善

--- src/browser/dashboard/views/camera.tsx
+++ src/browser/dashboard/views/camera.tsx
@@ -1,42 +1,152 @@
 import "modern-normalize";
+import styled from "styled-components";
 import ReactDOM from "react-dom";
+import Button from "@material-ui/core/Button";
+import IconButton, {IconButtonProps} from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import CheckIcon from "@material-ui/icons/Check";
+import EditIcon from "@material-ui/icons/Edit";
+import UndoIcon from "@material-ui/icons/Undo";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import ListItemText from "@material-ui/core/ListItemText";
+import {useEffect, useState} from "react";
+
 import {useReplicant} from "../../use-replicant";
 
-const cameraNameRep = nodecg.Replicant("camera-name");
+const Container = styled.div`
+	padding: 8px;
+`;
+
+type ButtonProps = Pick<IconButtonProps, "onClick">;
+
+const EditButton = (props: ButtonProps) => {
+	return (
+		<IconButton {...props}>
+			<EditIcon />
+		</IconButton>
+	);
+};
+
+const SubmitButton = (props: ButtonProps) => {
+	return (
+		<IconButton {...props}>
+			<CheckIcon />
+		</IconButton>
+	);
+};
+
+const UndoButton = (props: ButtonProps) => {
+	return (
+		<IconButton {...props}>
+			<UndoIcon />
+		</IconButton>
+	);
+};
 
 const App = () => {
+	const cameraNameRep = nodecg.Replicant("camera-name");
 	const cameraName = useReplicant("camera-name");
+	const [edit, setEdit] = useState<boolean>(false);
+	const [cameraNameForm, setCameraNameForm] = useState({
+		title: "",
+		name: "",
+	});
 	const cameraState = useReplicant("camera-state");
+	useEffect(() => {
+		setCameraNameForm({
+			title: cameraName?.title ?? "",
+			name: cameraName?.name ?? "",
+		});
+	}, [cameraName]);
 
 	return (
-		<div
-			style={{
-				padding: "8px",
-			}}
-		>
-			<label>名前</label>
-			<input
-				type='text'
-				value={cameraName?.name || ""}
-				onChange={(e) => {
-					if (cameraNameRep.value) {
-						cameraNameRep.value.name = e.target.value;
-					}
-				}}
-			/>
-			<button
+		<Container>
+			<List>
+				<ListItem>
+					<ListItemText
+						primary={
+							edit ? (
+								<input
+									value={cameraNameForm?.title}
+									onChange={(e) => {
+										setCameraNameForm({
+											...cameraNameForm,
+											title: e.currentTarget.value,
+										});
+									}}
+									placeholder='タイトル'
+								/>
+							) : (
+								`タイトル : ${cameraNameForm.title}`
+							)
+						}
+						secondary={
+							edit ? (
+								<input
+									value={cameraNameForm?.name}
+									onChange={(e) => {
+										setCameraNameForm({
+											...cameraNameForm,
+											name: e.currentTarget.value,
+										});
+									}}
+									placeholder='名前'
+								/>
+							) : (
+								<Typography variant='body2'>{`名前 : ${cameraNameForm.name}`}</Typography>
+							)
+						}
+					/>
+					<ListItemSecondaryAction>
+						{edit ? (
+							<>
+								<SubmitButton
+									onClick={() => {
+										if (cameraNameRep.value) {
+											cameraNameRep.value = {...cameraNameForm};
+										}
+										setEdit(false);
+									}}
+								/>
+								/
+								<UndoButton
+									onClick={() => {
+										setCameraNameForm({
+											title: cameraName?.title ?? "",
+											name: cameraName?.name ?? "",
+										});
+										setEdit(false);
+									}}
+								/>
+							</>
+						) : (
+							<>
+								<EditButton
+									onClick={() => {
+										setEdit(true);
+									}}
+								/>
+							</>
+						)}
+					</ListItemSecondaryAction>
+				</ListItem>
+			</List>
+			<Button
 				onClick={() => {
 					nodecg.sendMessage("toggleCameraName");
 				}}
 				disabled={cameraState === "big"}
+				variant='outlined'
 			>
 				{cameraState === "hidden"
 					? "表示する"
 					: cameraState === "big"
 					? "表示中(大)"
 					: "隠す"}
-			</button>
-		</div>
+			</Button>
+		</Container>
 	);
 };
 
