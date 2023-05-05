14845eaf0104944670f3d1571fa1a8dab7f8f596
Keiichiro Amemiya <hoishinxii@gmail.com>
2023-05-06 01:07:03 +0900

    chore: remove unnecessary type

--- src/browser/global.d.ts
+++ src/browser/global.d.ts
@@ -3,13 +3,6 @@ import {Configschema} from "../nodecg/generated/configschema";
 import {ReplicantMap} from "../nodecg/replicants";
 import {MessageMap} from "../nodecg/messages";
 
-export interface FontFaceSet {
-	status: "loading" | "loaded";
-	ready: Promise<FontFaceSet>;
-	check(font: string, text?: string): boolean;
-	load(font: string, text?: string): Promise<void>;
-}
-
 declare global {
 	const nodecg: CreateNodecgInstance<
 		"rtainjapan-layouts",
