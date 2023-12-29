98c491066ff0a02002a25d2f76ce68a6f1b977ce
Cma <33190610+cma2819@users.noreply.github.com>
2023-12-29 14:32:07 +0900

    ネームプレートの同期がズレるのを修正した (#743)

---
+++ src/browser/graphics/components/nameplate/sync-display.tsx
@@ -0,0 +1,49 @@
+import gsap from "gsap";
+import {ReactNode, createContext, useEffect, useState} from "react";
+import {useReplicant} from "../../../use-replicant";
+
+type DisplayLabel = "name" | "twitter" | "twitch" | "nico";
+
+export const SyncDisplayContext = createContext<DisplayLabel>("name");
+
+export const SyncDisplayProvider = ({children}: {children: ReactNode}) => {
+	const [display, setDisplay] = useState<DisplayLabel>("name");
+
+	const currentRun = useReplicant("current-run");
+	const participants = [
+		...(currentRun?.runners ?? []),
+		...(currentRun?.commentators ?? []),
+	];
+
+	const displayTwitter = participants.some((p) => Boolean(p?.twitter));
+	const displayTwitch = participants.some((p) => Boolean(p?.twitch));
+	const displayNico = participants.some((p) => Boolean(p?.nico));
+
+	useEffect(() => {
+		const tl = gsap.timeline({repeat: -1});
+		const displays: DisplayLabel[] = [
+			displayTwitter ? "twitter" : null,
+			displayTwitch ? "twitch" : null,
+			displayNico ? "nico" : null,
+			"name",
+		].filter((v): v is DisplayLabel => v !== null);
+		for (const social of displays) {
+			tl.call(
+				(s) => {
+					setDisplay(s);
+				},
+				[social],
+				"+=31", // 表示時間の30秒と切り替え時間の1秒
+			);
+		}
+		return () => {
+			tl.kill();
+		};
+	}, [displayTwitter, displayTwitch, displayNico]);
+
+	return (
+		<SyncDisplayContext.Provider value={display}>
+			{children}
+		</SyncDisplayContext.Provider>
+	);
+};
